cbfcbac3379935ed907191e3f4fe692049b70677
FHaze <eder_arm@icloud.com>
2017-08-29 16:54:58 -0300

    Commit 5

--- FlandreSVGTool.py
+++ FlandreSVGTool.py
@@ -1,5 +1,8 @@
+import json
 import os
 import sys
+import xml.etree.ElementTree
+
 from PyQt5 import QtCore, QtWidgets
 
 from PyQt5.QtCore import QThread
@@ -28,9 +31,9 @@ class SvgTool(QMainWindow):
     inputFiles = []
     modes = [
         PresetMode(0, "Custom", "", "", True, [1, 2, 3], [1, 1.5, 2, 3], True),
-        PresetMode(1, "iOS Launcher Icon", "", "", True, [29, 40, 50, 57, 58, 72, 76, 80, 87, 100, 114, 120, 144, 152, 167, 180], [], False),
+        PresetMode(1, "iOS Launcher Icon", 0, 0, True, [29, 40, 50, 57, 58, 72, 76, 80, 87, 100, 114, 120, 144, 152, 167, 180], [], False),
         PresetMode(2, "Android Launcher Icon", 48, 48, True, [], [1, 1.5, 2, 3], True),
-        PresetMode(3, "Store Icon", "", "", True, [1024], [512], False),
+        PresetMode(3, "Store Icon", 0, 0, True, [1024], [512], False),
         PresetMode(4, "Button Icon", 16, 16, True, [1, 2, 3], [1, 1.5, 2, 3], True),
         PresetMode(5, "Big Button Icon", 24, 24, True, [1, 2, 3], [1, 1.5, 2, 3], True)
     ]
@@ -107,7 +110,9 @@ class SvgTool(QMainWindow):
             self.ui.lineOutputDir.text(),
             float(self.ui.lineWidth.text()),
             float(self.ui.lineHeight.text()),
-            self.isMultiplier
+            self.isMultiplier,
+            self.ui.checkBoxXcassets.isChecked(),
+            self.ui.checkBoxVS.isChecked()
         )
         self.convertProgress.sigSetProgress.connect(self.setProgress)
         self.convertProgress.sigSetProgressTotal.connect(self.setProgressTotal)
@@ -194,6 +199,8 @@ class SvgTool(QMainWindow):
         self.ui.checkBoxRatio.setEnabled(not state)
         self.ui.checkBoxAndroid.setEnabled(not state)
         self.ui.checkBoxIos.setEnabled(not state)
+        self.ui.checkBoxXcassets.setEnabled(not state)
+        self.ui.checkBoxVS.setEnabled(not state)
         self.ui.comboBoxMode.setEnabled(not state)
 
     def populateModes(self):
@@ -210,7 +217,7 @@ class SvgConversion(QThread):
 
     cancelToken = False
 
-    def __init__(self, inputFiles, convertAndroid, convertIos, androidSizeList, iosSizeList, inputDir, outputDir, baseWidth, baseHeight, isMultiplier):
+    def __init__(self, inputFiles, convertAndroid, convertIos, androidSizeList, iosSizeList, inputDir, outputDir, baseWidth, baseHeight, isMultiplier, isXCAssets, isUpdateSolution):
         super().__init__()
         self.inputFiles = inputFiles
         self.convertAndroid = convertAndroid
@@ -222,6 +229,8 @@ class SvgConversion(QThread):
         self.baseWidth = baseWidth
         self.baseHeight = baseHeight
         self.isMultiplier = isMultiplier
+        self.isXCAssets = isXCAssets
+        self.isUpdateSolution = isUpdateSolution
 
     def cancel(self):
         self.cancelToken = True
@@ -229,11 +238,15 @@ class SvgConversion(QThread):
     def run(self):
         fileCurrent = 0
         fileCount = len(self.inputFiles)
+        iosXCAssets = ""
+
+        self.createDirectories()
 
         for file in self.inputFiles:
             if self.cancelToken:
                 break
 
+            fileWithoutExt = str(file).replace(".svg", "")
             convertCurrent = 0
             convertCount = 0
 
@@ -246,9 +259,28 @@ class SvgConversion(QThread):
 
             infile = "{0}/{1}".format(self.inputDir, file)
             self.sigSetProgressTotal.emit((fileCurrent / fileCount) * 100)
+            if self.isXCAssets:
+                if self.isMultiplier:
+                    contentjson = {
+                        'images': [{'idiom': "universal"}],
+                        'properties': {'template-rendering-intent': ""},
+                        'info': {'version': 1, 'author': "xcode"}
+                    }
+                else:
+                    contentjson = {
+                        'images': [],
+                        'info': {'version': 1, 'author': "xcode"}
+                    }
 
             if self.convertIos:
-                self.sigSetStatusMessage.emit('Converting iOS({0}/{1}): "{2}"'.format(fileCurrent, fileCount, infile))
+                if self.isUpdateSolution:
+                    xml.etree.ElementTree.register_namespace("", "http://schemas.microsoft.com/developer/msbuild/2003")
+                    iOSSolution = self.findIosSolutionFile()
+                    iOSSolutionXml = xml.etree.ElementTree.parse(iOSSolution)
+                    root = iOSSolutionXml.getroot()
+                    iositemgroup = root.findall("{http://schemas.microsoft.com/developer/msbuild/2003}ItemGroup")[1]
+
+                self.sigSetStatusMessage.emit('Exporting iOS({0}/{1}): "{2}"'.format(fileCurrent, fileCount, infile))
                 for size in self.iosSizeList:
                     if self.cancelToken:
                         break
@@ -256,23 +288,60 @@ class SvgConversion(QThread):
                     convertCurrent += 1
 
                     if self.isMultiplier:
-                        if size == 3:
-                            outfile = "{0}/iOS/Resources/{1}".format(self.outputDir,
-                                                                     str.replace(file, ".svg", "@3x.png"))
-                        elif size == 2:
-                            outfile = "{0}/iOS/Resources/{1}".format(self.outputDir,
-                                                                     str.replace(file, ".svg", "@2x.png"))
-                        else:
-                            outfile = "{0}/iOS/Resources/{1}".format(self.outputDir,
-                                                                     str.replace(file, ".svg", ".png"))
-
                         width = size * int(self.baseWidth)
                         height = size * int(self.baseHeight)
-                    else:
-                        outfile = "{0}/iOS/Resource/{1}".format(self.outputDir, str.replace(file, ".svg", "_{0}.png".format(str(size))))
 
+                        if size == 3:
+                            outfilename = str.replace(file, ".svg", "@3x.png")
+                        elif size == 2:
+                            outfilename = str.replace(file, ".svg", "@2x.png")
+                        else:
+                            outfilename = str.replace(file, ".svg", ".png")
+                    else:
                         width = size
                         height = size
+                        outfilename = "{0}_{1}.png".format(str.replace(file, ".svg", ""), size)
+
+                    if self.isXCAssets:
+                        if self.isMultiplier:
+                            iosXCAssets = "/Assets.xcassets/{0}.imageset".format(fileWithoutExt)
+                            contentjson['images'].append(
+                                {'scale': "{0}x".format(size), 'idiom': "universal", "filename": outfilename})
+                        else:
+                            iosXCAssets = "/Assets.xcassets/{0}.appiconset".format(fileWithoutExt)
+                            elems = self.getiosappiconjson(outfilename, size)
+                            for elem in elems:
+                                contentjson['images'].append(elem)
+
+                        outfile = "{0}/iOS{1}/{2}".format(self.outputDir, iosXCAssets, outfilename)
+                        outcontentjson = "{0}/iOS{1}/{2}".format(self.outputDir, iosXCAssets, "Contents.json")
+
+                        if not os.path.exists("{0}/iOS{1}".format(self.outputDir, iosXCAssets)):
+                            os.makedirs("{0}/iOS{1}".format(self.outputDir, iosXCAssets))
+
+                        if os.path.exists(outcontentjson):
+                            os.remove(outcontentjson)
+
+                        with open(outcontentjson, 'w') as outjson:
+                            json.dump(contentjson, outjson)
+                    else:
+                        outfile = "{0}/iOS/Resources/{1}".format(self.outputDir, outfilename)
+
+                    if self.isUpdateSolution:
+                        doInclude = True
+                        iosinclude = outfile.split("/iOS/")[1].replace("/", "\\").replace("@", "%40")
+
+                        for item in iositemgroup:
+                            if item.attrib["Include"] == iosinclude:
+                                doInclude = False
+                                break
+
+                        if doInclude:
+                            newitem = xml.etree.ElementTree.Element("ImageAsset")
+                            newitem.set("Include", outfile.split("/iOS/")[1].replace("/", "\\"))
+                            iositemgroup.append(newitem)
+                            iOSSolutionXml.write(iOSSolution)
+
 
                     if os.path.isfile(outfile):
                         os.remove(outfile)
@@ -280,8 +349,30 @@ class SvgConversion(QThread):
                     self.sigSetProgress.emit((convertCurrent / convertCount) * 100)
                     os.system('svg2png "{0}" -o "{1}" -w {2} -h {3}'.format(infile, outfile, width, height))
 
+                if self.isXCAssets:
+                    doInclude = True
+                    ioscontentinclude = outcontentjson.split("/iOS/")[1]
+
+                    for item in iositemgroup:
+                        if item.attrib["Include"] == ioscontentinclude:
+                            doInclude = False
+                            break
+
+                    if doInclude:
+                        newitem = xml.etree.ElementTree.Element("ImageAsset")
+                        newitem.set("Include", ioscontentinclude.replace("/", "\\"))
+                        iositemgroup.append(newitem)
+                        iOSSolutionXml.write(iOSSolution)
+
             if self.convertAndroid:
-                self.sigSetStatusMessage.emit('Converting Droid({0}/{1}): "{2}"'.format(fileCurrent, fileCount, infile))
+                if self.isUpdateSolution:
+                    xml.etree.ElementTree.register_namespace("", "http://schemas.microsoft.com/developer/msbuild/2003")
+                    androidSolution = self.findAndroidSolutionFile()
+                    androidSolutionXml = xml.etree.ElementTree.parse(androidSolution)
+                    root = androidSolutionXml.getroot()
+                    androiditemgroup = root.findall("{http://schemas.microsoft.com/developer/msbuild/2003}ItemGroup")[3]
+
+                self.sigSetStatusMessage.emit('Exporting Droid({0}/{1}): "{2}"'.format(fileCurrent, fileCount, infile))
                 for size in self.androidSizeList:
                     if self.cancelToken:
                         break
@@ -305,13 +396,28 @@ class SvgConversion(QThread):
                         width = size * self.baseWidth
                         height = size * self.baseHeight
                     else:
-                        outfile = "{0}/Droid/Resource/{1}".format(self.outputDir, str.replace(file, ".svg", "_{0}".format(str(size))))
+                        outfile = "{0}/Droid/Resources/{1}".format(self.outputDir, str.replace(file, ".svg", "_{0}".format(str(size))))
 
                         width = size
                         height = size
 
                     if os.path.isfile(outfile):
                         os.remove(outfile)
+
+                    if self.isUpdateSolution:
+                        doInclude = True
+                        androidinclude = outfile.split("/Droid/")[1].replace("/", "\\")
+
+                        for item in androiditemgroup:
+                            if item.attrib["Include"] == androidinclude:
+                                doInclude = False
+                                break
+
+                        if doInclude:
+                            newitem = xml.etree.ElementTree.Element("AndroidResource")
+                            newitem.set("Include", outfile.split("/Droid/")[1].replace("/", "\\"))
+                            androiditemgroup.append(newitem)
+                            androidSolutionXml.write(androidSolution)
 
                     self.sigSetProgress.emit((convertCurrent / convertCount) * 100)
                     os.system('svg2png "{0}" -o "{1}" -w {2} -h {3}'.format(infile, outfile, width, height))
@@ -325,6 +431,178 @@ class SvgConversion(QThread):
 
         self.sigSetStatusMessage.emit("Flandre finished her job!")
         self.sigSetUiInProgress.emit(False)
+
+    def findAndroidSolutionFile(self):
+        for file in os.listdir("{0}/Droid".format(self.outputDir)):
+            if file.endswith(".csproj"):
+                return "{0}/Droid/{1}".format(self.outputDir, file)
+
+    def findIosSolutionFile(self):
+        for file in os.listdir("{0}/iOS".format(self.outputDir)):
+            if file.endswith(".csproj"):
+                return "{0}/iOS/{1}".format(self.outputDir, file)
+
+    def createDirectories(self):
+        directories = [
+            "/Droid",
+            "/Droid/Resources",
+            "/Droid/Resources/drawable",
+            "/Droid/Resources/drawable-hdpi",
+            "/Droid/Resources/drawable-xhdpi",
+            "/Droid/Resources/drawable-xxhdpi",
+            "/iOS"
+        ]
+
+        if self.isXCAssets:
+            directories.append("/iOS/Assets.xcassets")
+        else:
+            directories.append("/iOS/Resources")
+
+        for directory in directories:
+            if not os.path.exists(self.outputDir + directory):
+                os.makedirs(self.outputDir + directory)
+
+    def getiosappiconjson(self, filename, size):
+        elems = []
+        baseelem = {"filename": filename}
+
+        if size == 29:
+            newelem = baseelem.copy()
+            newelem["size"] = "29x29"
+            newelem["scale"] = "1x"
+            newelem["idiom"] = "iphone"
+            elems.append(newelem)
+
+            newelem = baseelem.copy()
+            newelem["size"] = "29x29"
+            newelem["scale"] = "1x"
+            newelem["idiom"] = "ipad"
+            elems.append(newelem)
+
+        elif size == 40:
+            newelem = baseelem.copy()
+            newelem["size"] = "40x40"
+            newelem["scale"] = "1x"
+            newelem["idiom"] = "ipad"
+            elems.append(newelem)
+
+        elif size == 50:
+            newelem = baseelem.copy()
+            newelem["size"] = "50x50"
+            newelem["scale"] = "1x"
+            newelem["idiom"] = "ipad"
+            elems.append(newelem)
+
+        elif size == 57:
+            newelem = baseelem.copy()
+            newelem["size"] = "57x57"
+            newelem["scale"] = "1x"
+            newelem["idiom"] = "iphone"
+            elems.append(newelem)
+
+        elif size == 58:
+            newelem = baseelem.copy()
+            newelem["size"] = "29x29"
+            newelem["scale"] = "2x"
+            newelem["idiom"] = "iphone"
+            elems.append(newelem)
+
+            newelem = baseelem.copy()
+            newelem["size"] = "29x29"
+            newelem["scale"] = "2x"
+            newelem["idiom"] = "ipad"
+            elems.append(newelem)
+
+        elif size == 72:
+            newelem = baseelem.copy()
+            newelem["size"] = "72x72"
+            newelem["scale"] = "1x"
+            newelem["idiom"] = "ipad"
+            elems.append(newelem)
+
+        elif size == 76:
+            newelem = baseelem.copy()
+            newelem["size"] = "76x76"
+            newelem["scale"] = "1x"
+            newelem["idiom"] = "ipad"
+            elems.append(newelem)
+
+        elif size == 80:
+            newelem = baseelem.copy()
+            newelem["size"] = "40x40"
+            newelem["scale"] = "2x"
+            newelem["idiom"] = "iphone"
+            elems.append(newelem)
+
+            newelem = baseelem.copy()
+            newelem["size"] = "40x40"
+            newelem["scale"] = "2x"
+            newelem["idiom"] = "ipad"
+            elems.append(newelem)
+
+        elif size == 87:
+            newelem = baseelem.copy()
+            newelem["size"] = "29x29"
+            newelem["scale"] = "3x"
+            newelem["idiom"] = "iphone"
+            elems.append(newelem)
+
+        elif size == 100:
+            newelem = baseelem.copy()
+            newelem["size"] = "50x50"
+            newelem["scale"] = "2x"
+            newelem["idiom"] = "ipad"
+            elems.append(newelem)
+
+        elif size == 114:
+            newelem = baseelem.copy()
+            newelem["size"] = "57x57"
+            newelem["scale"] = "2x"
+            newelem["idiom"] = "iphone"
+            elems.append(newelem)
+
+        elif size == 120:
+            newelem = baseelem.copy()
+            newelem["size"] = "40x40"
+            newelem["scale"] = "3x"
+            newelem["idiom"] = "iphone"
+            elems.append(newelem)
+
+            newelem = baseelem.copy()
+            newelem["size"] = "60x60"
+            newelem["scale"] = "2x"
+            newelem["idiom"] = "iphone"
+            elems.append(newelem)
+
+        elif size == 144:
+            newelem = baseelem.copy()
+            newelem["size"] = "72x72"
+            newelem["scale"] = "2x"
+            newelem["idiom"] = "ipad"
+            elems.append(newelem)
+
+        elif size == 152:
+            newelem = baseelem.copy()
+            newelem["size"] = "76x76"
+            newelem["scale"] = "2x"
+            newelem["idiom"] = "ipad"
+            elems.append(newelem)
+
+        elif size == 167:
+            newelem = baseelem.copy()
+            newelem["size"] = "83.5x83.5"
+            newelem["scale"] = "2x"
+            newelem["idiom"] = "ipad"
+            elems.append(newelem)
+
+        elif size == 180:
+            newelem = baseelem.copy()
+            newelem["size"] = "60x60"
+            newelem["scale"] = "3x"
+            newelem["idiom"] = "iphone"
+            elems.append(newelem)
+
+        return elems
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
